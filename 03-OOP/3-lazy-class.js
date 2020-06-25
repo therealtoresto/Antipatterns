98ea86d31beaec718701cc55919f89dd7264f8cc
TORESTO <therealtoresto@gmail.com>
2020-06-25 17:49:42 +0300

    Lazy Class antipattern

---
+++ 03-OOP/3-lazy-class.js
@@ -0,0 +1,25 @@
+'use strict';
+
+// Antipattern: Lazy Class
+
+class PersonName {
+    constructor(name) {
+        this.name = name;
+    }
+}
+
+class PersonBirth {
+    constructor(birth) {
+        this.birth = new Date(birth);
+    }
+}
+
+class Person {
+    constructor(name, birth) {
+        this.name = new PersonName(name);
+        this.birth = new PersonBirth(birth);
+    }
+}
+
+const person = new Person('Marcus Aurelius', '121-04-26');
+console.dir({ person });
